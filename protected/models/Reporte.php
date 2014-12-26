3d0ef22a52c4587b32685ccd09be0096558f5f5b
luis2000 <luxdao.scal@gmail.com>
2014-12-26 13:15:53 -0400

    reporte

---
+++ protected/models/Reporte.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luis
+ * Date: 26/12/14
+ * Time: 12:20
+ */
+
+class Reporte {
+    public $id_empleado;
+    public $per;
+    public $uni;
+    public $car;
+    public $lista_jornada;
+    public $totaltrabajado;
+    public $totaltiempoextra;
+    public $totaltiemporetrazo;
+    public $totaldiastrabajados;
+    function  Reporte($id_persona,$fechainicio,$fechafin){
+
+    }
+}
