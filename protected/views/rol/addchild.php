878833af5a551497a5c81b1fa89a892e764b0a84
luis2000 <luxdao.scal@gmail.com>
2015-08-06 20:57:44 -0400

    Merge branch 'master' of C:\wamp\www\SICCSAP2.0 with conflicts.
    usuarios version gamma

--- protected/views/rol/addchild.php
+++ protected/views/rol/addchild.php
@@ -13,39 +13,91 @@ $this->menu=array(
 );
 ?>
 
+<?php
+Yii::app()->clientScript->registerScript('search','
+    $("#roles-grid").hide();
+    function getroles(name){
+        $("#nombrerol").prop("value",name);
+        $("#usuarios-grid").slideToggle(400);
+    };
+    $("#nombrerol").on("click",function(){
+        $("#roles-grid").slideToggle(400);
+        $(this).prop("maxlength","0");
+        $(this).prop("placeholder","Escoger un rol de la siguiente tabla:");
+    });
+');
+
+?>
+
+
 <h1>Asignar Rol</h1>
 
-<div class="form">
 
-    <?php $form=$this->beginWidget('CActiveForm', array(
-        'id'=>'auth-assignment-form',
-        // Please note: When you enable ajax validation, make sure the corresponding
-        // controller action is handling ajax validation correctly.
-        // There is a call to performAjaxValidation() commented in generated controller code.
-        // See class documentation of CActiveForm for details on this.
-        'enableAjaxValidation'=>false,
-    )); ?>
 
-    <p class="note">Fields with <span class="required">*</span> are required.</p>
+<div class="row"">
+<div class="col-md-12">
 
-    <?php echo $form->errorSummary($model); ?>
+    <?php
+    /* @var $this AuthItemController */
+    /* @var $model AuthItem */
 
-    <div class="row">
-        <?php echo $form->labelEx($model,'parent'); ?>
-        <?php echo $form->dropDownList($model,'parent',$this->getRoles()); ?>
-        <?php echo $form->error($model,'parent'); ?>
+    $this->breadcrumbs=array(
+        'Auth Items'=>array('index'),
+        'Create',
+    );
+
+    $this->menu=array(
+        array('label'=>'List AuthItem', 'url'=>array('index')),
+        array('label'=>'Manage AuthItem', 'url'=>array('admin')),
+    );
+    ?>
+    <div class="box box-primary">
+        <div class="box-header">
+            <h2>Asignar SubRoles</h2>
+        </div>
+        <div class="box-body">
+            <div class="form">
+                <?php echo CHtml::beginForm(); ?>
+
+                <div class="form-group">
+                    <label> Nombre Rol</label>
+                    <input type="text" id="nombrerol" name="nombrerol" class="form-control"/>
+                    </div>
+                    <?php $this->widget('zii.widgets.grid.CGridView', array(
+                        'id'=>'roles-grid',
+                        'dataProvider'=>$roles  ,
+                        'itemsCssClass' => 'table table-bordered table-striped dataTable',
+                        'columns'=>array(
+                            'name',
+                            'description',
+                        ),
+                        'selectionChanged'=>'function(name){getroles($.fn.yiiGridView.getSelection(name))}',
+                    )); ?>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box box-primary box-solid">
+                            <div class="box-header"><h4> Seleccionar Roles para asignar</h4></div>
+                            <div class="box-body">
+                                <?php foreach($items as $i=>$item): ?>
+                                    <div class="col-md-3">
+                                        <div class="checkbox">
+                                            <label><input type="checkbox" name="roles[]" value=<?php echo $item->name;?> /><?php echo $item->name;?></label>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    <?php echo CHtml::submitButton('Save',array('class'=>'btn btn-primary btn-lg')); ?>
+                </div>
+                <?php echo CHtml::endForm(); ?>
+            </div><!-- form -->
+        </div>
     </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($model,'child'); ?>
-        <?php echo $form->dropDownList($model,'child',$this->getRoles()); ?>
-        <?php echo $form->error($model,'child'); ?>
-    </div>
-
-    <div class="row buttons">
-        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-    </div>
-
-    <?php $this->endWidget(); ?>
-
+</div>
 </div>
